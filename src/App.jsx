7dda05d98b68942500dbfd8173dbea3f385f1a98
sag-yadav <sagaryadav15052005@gmail.com>
2024-08-13 03:41:33 -0700

    main code react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-
-
+import { useEffect, useState } from 'react'
+import Navbar from './Components/Navbar'
+import Carsouel from './Components/Carsouel'
+import Card from './Components/Card'
+import Fotter from './Components/Fotter'
+import Signup from './Components/Signup'
+import { Route, BrowserRouter as Router,Routes} from "react-router-dom"
+import Login from './Components/Login'
+import Accord from './Components/Accord'
 function App() {
-
-
+  
   return (
     <>
-    <h1>sagar</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Accord />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/carsouel" element={<Carsouel />} />
+        <Route path="/fotter" element={<Fotter />} />
+        <Route path="/login" element={<Login />} />
+        
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/Components/Accord.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Accord() {
+  return (
+<>
+ 
+<>
+  <div className="d-inline p-2 text-bg-primary">d-inline</div>
+  <div className="d-inline p-2 text-bg-dark">d-inline</div>
+</>
+
+
+  {/*<div className="mb-4">
+    <label htmlFor="exampleFormControlInput1" className="form-label">
+      Email address
+    </label>
+    <input
+      type="email"
+      className="form-control border-success"
+      id="exampleFormControlInput1"
+      placeholder="name@example.com"
+    />
+  </div>
+  <div className="h4 pb-2 mb-4 text-danger border-bottom border-danger">
+    Dangerous heading
+  </div>
+  <div className="p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
+    Changing border color and width
+  </div>*/}
+
+ {/* <div className="bg-success p-2 text-white">
+    This is default success background
+  </div>
+  <div className="bg-success p-2 text-white bg-opacity-75">
+    This is 75% opacity success background
+  </div>
+  <div className="bg-success p-2 text-dark bg-opacity-50">
+    This is 50% opacity success background
+  </div>
+  <div className="bg-success p-2 text-dark bg-opacity-25">
+    This is 25% opacity success background
+  </div>
+  <div className="bg-success p-2 text-dark bg-opacity-10">
+    This is 10% opacity success background
+  </div>*/}
+</>
+ 
+  )
+}
+
+export default Accord
